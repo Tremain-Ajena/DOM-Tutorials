c111ab9d8c77526ea28c6cb7b8cb7820ea7a027e
IB_Student <student@InnovateBhams-MacBook-Pro-2.local>
2020-01-23 17:05:49 -0600

    first upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let paragraph = document.createElement('p');
+    let text = document.createTextNode('this can be whatever you want it to be');
+
+    paragraph.style.color = "red";
+    paragraph.style.textTransform = "uppercase";
+    paragraph.className = "some-paragraph";
+
+    paragraph.appendChild(text);
+    document.body.appendChild(paragraph);
+
+    let button = document.createElement('button');
+    let btnText = document.createTextNode('Click Me!');
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+
+    button.addEventListener('click', function () {
+        let h1 = document.createElement('h1');
+        let h1Text = document.createTextNode('I have been clicked!');
+        h1.appendChild(h1Text);
+        document.body.appendChild(h1);
+    })
+});
